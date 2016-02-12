6a1ad09f45b188231045c4ad3ad21e7159496b93
Nizar Kury <nkury@asu.edu>
2016-02-12 14:11:01 -0700

    system call code v2
    Instead of defining my own data structure, I made four buffers instead
    storing all the information

--- my_syscall.c
+++ my_syscall.c
@@ -6,26 +6,30 @@
 #include <linux/jiffies.h>
 #include "ProcessInfo.h"
 
-asmlinkage long sys_my_syscall(int a, int b, process_info* list){
+asmlinkage long sys_my_syscall(int a, int b, int* pids, int* times, char &comm[][16], char &name[][64]){
 
-	// i may need to use malloc? I'm not sure on that front
-	struct process_info buffer[1024]; // define the buffer that will communicate info between user-space and kernel-space
+	int taskPIDs[10000];
+	int taskTimes[10000];
+	char taskComms[10000][16];
+	char taskNames[10000][64];
+
 	int count = 0;
 	struct task_struct *task;	// instantiate an instance of task from task_struct that will be traversed in the following loop
 
 	// we loop through each of the tasks
 	for_each_process(task){
-		struct process_info instance; // for storing information of a current task/process
-		instance.pid = task->pid;
+		taskPIDs[count] = task->pid;
 		long userTime = task->utime / HZ; // convert user time (in jiffies) to seconds
 		long execTime = task->stime / HZ; // convert exec time (in jiffies) to seconds
-		instance.time_in_seconds = userTime + execTime; // sum of user and exec time is process time
-		instance.comm = task->comm;
-		instance.name = task->signal->tty->name;
-		buffer[count] = instance;
+		taskTimes[count] = userTime + execTime; // sum of user and exec time is process time
+		taskComms[count] = task->comm;
+		taskNames[count] = task->signal->tty->name;
 		count++;
 	}
 
-	int n = copy_to_user(list, buffer, sizeof(process_info)); // use copy_to_user to actually copy kernel info to user space
-	return n; // returns 0 if copied successfully
+	int a = copy_to_user(pids, taskPIDS, sizeof(int)); // use copy_to_user to actually copy kernel info to user space
+	int b = copy_to_user(times, taskTimes, sizeof(int)); // use copy_to_user to actually copy kernel info to user space
+	int c = copy_to_user(comm, taskComms, sizeof(taskComms)); // use copy_to_user to actually copy kernel info to user space
+	int d = copy_to_user(name, taskNames, sizeof(taskNames)); // use copy_to_user to actually copy kernel info to user space
+	return a + b + c + d; // returns 0 if copied successfully
 }
