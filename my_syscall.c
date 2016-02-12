d063ebfafecdb4b3d579cda15946242256996d4f
Nizar Kury <nkury@asu.edu>
2016-02-12 14:38:55 -0700

    System call v3

--- my_syscall.c
+++ my_syscall.c
@@ -4,14 +4,14 @@
 #include <linux/list.h>
 #include <linux/tty.h>
 #include <linux/jiffies.h>
-#include "ProcessInfo.h"
 
-asmlinkage long sys_my_syscall(int a, int b, int* pids, int* times, char &comm[][16], char &name[][64]){
+
+asmlinkage long sys_my_syscall(int a, int b, int* pids, int* times, char* comm[], char* name[]){
 
 	int taskPIDs[10000];
 	int taskTimes[10000];
-	char taskComms[10000][16];
-	char taskNames[10000][64];
+	char* taskComms[10000];
+	char* taskNames[10000];
 
 	int count = 0;
 	struct task_struct *task;	// instantiate an instance of task from task_struct that will be traversed in the following loop
@@ -27,9 +27,9 @@ asmlinkage long sys_my_syscall(int a, int b, int* pids, int* times, char &comm[]
 		count++;
 	}
 
-	int a = copy_to_user(pids, taskPIDS, sizeof(int)); // use copy_to_user to actually copy kernel info to user space
-	int b = copy_to_user(times, taskTimes, sizeof(int)); // use copy_to_user to actually copy kernel info to user space
+	int x = copy_to_user(pids, taskPIDS, sizeof(int)); // use copy_to_user to actually copy kernel info to user space
+	int y = copy_to_user(times, taskTimes, sizeof(int)); // use copy_to_user to actually copy kernel info to user space
 	int c = copy_to_user(comm, taskComms, sizeof(taskComms)); // use copy_to_user to actually copy kernel info to user space
 	int d = copy_to_user(name, taskNames, sizeof(taskNames)); // use copy_to_user to actually copy kernel info to user space
-	return a + b + c + d; // returns 0 if copied successfully
+	return x + y + c + d; // returns 0 if copied successfully
 }
