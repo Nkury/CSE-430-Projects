04c4fba6286187501bc3272f15dd8761d8a4da47
Nizar Kury <nkury@asu.edu>
2016-02-13 23:24:19 -0700

    Modified user.c and my_syscall.c
    Based on our discussion today, I modified the code accordingly

--- user.c
+++ user.c
@@ -1,23 +1,61 @@
-#include <linux/unistd.h>
+//#include <linux/unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
 #define __NR_my_syscall 359
+
+struct processInfo{
+	char id[16];
+	char time[16];
+	char comm[16];
+	char name[64];
+};
 
 int main()
 {
-	int a, b = 0;
-	int* user_pids;
-	int* user_times;
-	char* user_comm[];
-	char* user_name[];
-	
-	int ret = syscall(__NR_my_syscall, user_pids, user_times, user_comm, user_name);
-	
-	int i = 0;
-	
-	while (user_pids[i] != '\0' & user_times[i] != '\0' & user_comm[i] != '\0' & user_name[i] != '\0')
-	{
-			for(i = 0; i < 10000; i++)
-				printf("%d, %d, %s, %s\n", user_pids[i], user_times[i], user_comm[i], user_name[i]);
+	char buffer[1024] = ""; // initializes it to a value that is not null (?)
+	struct processInfo proc;
+	int index = 0;
+	printf("  PID\tTTY\t\t\tTIME\tCMD"); // header of the printed processes
+	while (buffer != NULL){
+		int ret = syscall(__NR_my_syscall, index, buffer);
+		if (buffer != NULL){
+			proc = parseProcess(buffer);
+			printf("  %s\t%s\t\t\t%s\t%s", proc.id, proc.name, proc.time, proc.comm);
+			index++;
+		}
 	}
-	
 	return 0;
+}
+
+// parse the space delimited buffer
+struct processInfo parseProcess(char buff[]){
+	char input[1024]; // to store input
+	struct processInfo proc;// = (struct processInfo)malloc(sizeof(struct processInfo));
+	// what to store
+	int selection = 0;
+	int lastIndex = 0;
+	int i;
+	for (i = 0; i < 1024; i++){
+		if (buff[i] != ' '){
+			input[i - lastIndex] = buff[i];
+			selection++;
+		}
+		else{
+			lastIndex = i+1; // +1 to take into account space
+			switch (selection){
+				case 0: strcpy(proc.id, input);
+					break;
+				case 1: strcpy(proc.name, input);
+					break;
+				case 2: strcpy(proc.time, input);
+					break;
+				case 3: strcpy(proc.comm, input);
+					break;
+			}
+			input = "";
+		}
+	}
+
+	return proc;
 }
