22e1e1239042f6eba75200d5a6ab71717f3592de
Jackson Curless <jcurless@asu.edu>
2016-02-13 20:51:07 -0700

    Update user.c

--- user.c
+++ user.c
@@ -3,21 +3,19 @@
 
 int main()
 {
-	int a, b = 0;
 	int* user_pids;
 	int* user_times;
-	char* user_comm[];
-	char* user_name[];
+	char* user_comm[100];
+	char* user_name[100];
 	
 	int ret = syscall(__NR_my_syscall, user_pids, user_times, user_comm, user_name);
 	
 	int i = 0;
 	
-	while (user_pids[i] != '\0' & user_times[i] != '\0' & user_comm[i] != '\0' & user_name[i] != '\0')
+	for(i = 0; i < 100; i++)
 	{
-			for(i = 0; i < 10000; i++)
-				printf("%d, %d, %s, %s\n", user_pids[i], user_times[i], user_comm[i], user_name[i]);
+		if (user_pids[i] != '\0' & user_times[i] != '\0' & user_comm[i] != '\0' & user_name[i] != '\0')
+			printf("%d, %d, %s, %s\n", user_pids[i], user_times[i], user_comm[i], user_name[i]);
 	}
-	
 	return 0;
 }
