7799363998977dd9128d6906d19df9984049830c
Jackson Curless <jcurless@asu.edu>
2016-02-22 21:55:02 -0700

    Update fork_bomb_killer.c
    Added #include <linux/sched.h>

--- fork_bomb_killer.c
+++ fork_bomb_killer.c
@@ -1,14 +1,15 @@
 #include <linux/module.h>	/* Needed by all modules */
 #include <linux/kernel.h>	/* Needed for KERN_INFO */
 #include <linux/init.h>		/* Needed for the macros */
+#include <linux/sched.h>	/* Needed for 'for_each_process' */
 
 static int __init fork_bomb_killer(void){
-	//printk(KERN_INFO "Nizar Kury\n");
+	printk(KERN_INFO "Fork Bomb Squad Deployed.\n");
 	return 0;
 }
 
 static void __exit fork_bomb_killer_exit(void){
-	//printk(KERN_INFO "Exiting...\n");
+	printk(KERN_INFO "Cleaning Up.\n");
 }
 
 module_init(fork_bomb_killer);
