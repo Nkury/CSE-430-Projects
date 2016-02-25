c9c8c8fbab8af7e15d5ff3e73f803ec3668b7e5b
Nizar Kury <nkury@asu.edu>
2016-02-24 23:28:31 -0700

    Added my code for traversing and locking tasks

--- fork_bomb_killer.c
+++ fork_bomb_killer.c
@@ -2,14 +2,45 @@
 #include <linux/kernel.h>	/* Needed for KERN_INFO */
 #include <linux/init.h>		/* Needed for the macros */
 #include <linux/sched.h>	/* Needed for 'for_each_process' */
+#include <linux/delay.h>
+#include <linux/list.h>
 
+struct task_struct *task;
+
+int my_kthread_function(void* data){
+	while (!kthread_should_stop()){
+		// most of the time kthread sleeps
+		msleep(1000);
+		read_lock(&tasklist_lock); // lock the task_struct
+		struct list_head *p;
+		struct task_struct ts;
+		for_each_process(task){
+			list_for_each(p, &(task->children)){
+				ts = *list_entry(p, struct task_struct, sibling);
+				//** do something with data structure Grant is creating
+			}
+		}
+		read_unlock(&tasklist_lock);
+		//** check Grant's data structure for depth greater than 3
+		//** call Jackson's kill command
+		//** stop the kthread somehow
+	}
+	return 0;
+}
 static int __init fork_bomb_killer(void){
-	printk(KERN_INFO "Fork Bomb Squad Deployed.\n");
+	data = 20;
+
+	// We can instantiate multiple threads, but I think one should suffice?
+	task = kthread_run(
+		&my_kthread_function,
+		(void*)data,
+		"my_kthread");
+
 	return 0;
 }
 
 static void __exit fork_bomb_killer_exit(void){
-	printk(KERN_INFO "Cleaning Up.\n");
+	kthread_stop(task);
 }
 
 module_init(fork_bomb_killer);
