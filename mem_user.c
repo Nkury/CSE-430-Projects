2057604f80998546af59cba8a504c3003f057985
Jackson Curless <jcurless@asu.edu>
2016-04-05 23:51:17 -0700

    Update mem_user.c

--- mem_user.c
+++ mem_user.c
@@ -8,23 +8,25 @@
 
 int main (int argc, char *argv[])
 {
-	int pid;
-	long virtAddr;
+	int pid = 0;
+	uint32_t virtAddr = 0;
+	char *p;
+	long address;
 	
 	if (argc != 3) {
-		printf("Two arguments are expected in the form of <PID> <0xVirtual_Address>.");
+		printf("Two arguments are expected in the form of <PID> <Virtual_Address>.");
 		exit(1);
 	}
 	else {
 		pid = atoi(argv[1]);
-		virtAddr = atoi(argv[2]);
-		long address = syscall(__NR_my_syscall, pid, virtAddr);
+		virtAddr = strtoul(argv[2], &p, 16);	// Not working
+		address = syscall(__NR_my_syscall, pid, virtAddr);
 	}
 
 	if (address == -1)
 		printf("That page is not available.\n");
 	else
-		printf("The address is %ld.", address);
+		printf("The address is %ld.\n", address);
 
 	return 0;
 }
