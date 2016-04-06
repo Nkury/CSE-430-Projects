2e65bd4b55b095d5ac51fab6dc179c50aaa656fe
Jackson Curless <jcurless@asu.edu>
2016-04-06 10:18:51 -0700

    Working

--- mem_user.c
+++ mem_user.c
@@ -1,17 +1,19 @@
 // mem_user.c test program
 
-#include <linux/unistd.h>
-#include <linux/stdio.h>
-#include <linux/stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <inttypes.h>
 
 #define __NR_my_syscall 359
 
 int main (int argc, char *argv[])
 {
 	int pid = 0;
-	uint32_t virtAddr = 0;
+	unsigned long virtAddr = 0;
 	char *p;
-	long address;
+	unsigned long address;
 	
 	if (argc != 3) {
 		printf("Two arguments are expected in the form of <PID> <Virtual_Address>.");
@@ -19,15 +21,18 @@ int main (int argc, char *argv[])
 	}
 	else {
 		pid = atoi(argv[1]);
-		virtAddr = strtoul(argv[2], &p, 16);	// Not working
+		virtAddr = strtoul(argv[2], &p, 16);
 		address = syscall(__NR_my_syscall, pid, virtAddr);
 	}
+
+	printf("pid: %d, ", pid);
+	printf("virtual address: 0x%lX, ", virtAddr);
 
 	if (address == -1)
 		printf("That page is not available.\n");
 	else {
-		printf("The address is %ld.\n", address);
-		printf("The address is 0x%lx.\n", address);
+		//printf("The address is %lu.\n", address);	// testing
+		printf("phys_addr: 0x%lX\n", address);
 	}
 
 	return 0;
