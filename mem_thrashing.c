41ab3f569ae2c05bd7806bf93bd095cbb0cb36a8
Nizar Kury <nkury@asu.edu>
2016-04-27 00:25:52 -0700

    Added some code

--- mem_thrashing.c
+++ mem_thrashing.c
@@ -25,87 +25,30 @@
 #include <asm/tlbflush.h>
 
 struct task_struct *task;
+int wss; // working set count
 
-int count = 0;
-int threshold = 100;
+int ptep_test_and_clear_young(struct vm_area_struct,
+	unsigned long, pte_t);
 
-struct processTable {
-	char name[64];
-	int pCount;
-	struct list_head list;
-};
-
-struct processTable myTable;
-
-bool checkName(char* name){
-	struct list_head *pos;
-	struct processTable *temp;
-	list_for_each(pos, &(myTable.list)){
-		temp = list_entry(pos, struct processTable, list);
-		if (strcmp(temp->name, name) == 0){
-			return false;
-		}
-	}
-	return true;
-}
-
-void kill_process(char* target);
 int my_kthread_function(void* data){
 
-	INIT_LIST_HEAD(&myTable.list);
+	pgd_t *pgd;
+	pud_t *pud;
+	pmd_t *pmd;
+	spinlock_t *ptl;
+	pte_t *ptep, pte;
+
 	while (!kthread_should_stop()){
 		struct list_head *p;
 		struct processTable *temp;
 		struct processTable *ts;
 		for_each_process(task){
-			if (task->pid != 1){
-				if (!checkName(task->comm)){
-					list_for_each(p, &(myTable.list)){
-						ts = list_entry(p, struct processTable, list);
-						if (strcmp(ts->name, task->comm) == 0){
-							ts->pCount++;
-						}
-					}
-				}
-				else{
-					temp = kmalloc(sizeof(struct processTable), GFP_KERNEL);
-					strcpy(temp->name, task->comm);
-					temp->pCount = 1;
-					list_add(&(temp->list), &(myTable.list));
-					// kfree(temp) not sure to put this in here
-				}
-			}
-		}
-		// checks to see if it can kill processes after a second
-		list_for_each(p, &(myTable.list)){
-			ts = list_entry(p, struct processTable, list);
-			if (ts->pCount > threshold){
-				kill_process(ts->name);
-			}
+
 		}
 
 		msleep(1000);
-		list_for_each(p, &(myTable.list)){
-			ts = list_entry(p, struct processTable, list);
-			ts->pCount = 0;
-		}
-
 	}
 	return 0;
-}
-
-// takes parameter name of target. Goes through all the tasks using
-// for_each_process and eliminates all processes that have the target
-// name
-
-void kill_process(char* target){
-
-	for_each_process(task) {
-		if (strcmp(task->comm, target) == 0) {
-			printk("KILLED: %d\n", task->pid);
-			send_sig(SIGKILL, task, 0);
-		}
-	}
 }
 
 static int __init thrash_detection(void){
@@ -118,6 +61,22 @@ static int __init thrash_detection(void){
 		"my_kthread");
 
 	return 0;
+}
+
+// checks if pte's access bit is 1. If it is, it will clear it appropriately
+int ptep_test_and_clear_young(struct vm_area_struct *vma,
+	unsigned long addr, pte_t *ptep)
+{
+	int ret = 0;
+
+	if (pte_young(*ptep))
+		ret = test_and_clear_bit(_PAGE_BIT_  ACCESSED,
+		(unsigned long *)&ptep->pte);
+
+	if (ret)
+		pte_update(vma - >vm_mm, addr, ptep);
+
+	return ret;
 }
 
 static void __exit thrash_detection_exit(void){
